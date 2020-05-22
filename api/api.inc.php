d3fb04f000d022984f7be3c4c6d0370f2659e712
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-22 16:08:43 +0100

    Initial api structure set up commit.

---
+++ api/api.inc.php
@@ -0,0 +1,7 @@
+<?php
+    //Including all internal API code
+    require_once("fn_bll.inc.php");
+    require_once("fn_pl.inc.php");
+    require_once("oo_bll.inc.php");
+    require_once("oo_master.inc.php");
+    require_once("oo_page.inc.php");
