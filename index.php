1d5ac767627071da2414c20c49897e8b48c10083
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-23 16:30:13 +0100

    Set up page logic for index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
+//INCLUDING API
+include("api/api.inc.php");
 
-echo "Hello World";
+//PAGE GENERATION LOGIC
+function createPage() {
+    $content = <<<PAGE
+<h1>Hello World from generated page</h1>
+PAGE;
+    return $content;
+}
+
+//BUSINESS LOGIC
+$pagetitle = "Home Page";
+$pagelead  = "";
+$pagecontent = createPage();
+$pagefooter = "";
+
+//BUILDING HTML PAGE
+$page = new MasterPage($pagetitle);
+if(!empty($pagelead))
+    $page->setDynamic1($pagelead);
+$page->setDynamic2($pagecontent);
+if(!empty($pagefooter))
+    $page->setDynamic3($pagefooter);
+$page->renderPage();
