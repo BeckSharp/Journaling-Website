4e2f1c3d3c1a874119e602f1eb687abedd88c01f
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-25 17:42:02 +0100

    Helper functions added to data access layer

--- api/fn_dal.inc.php
+++ api/fn_dal.inc.php
@@ -1,1 +1,37 @@
 <?php
+//Including the class definitions
+require_once("oo_bll.inc.php");
+
+//JSON HELPER FUNCTIONS
+function jsonOne($file,$id)
+{
+    $splfile = new SplFileObject($file);
+    $splfile->seek($id-1);
+    $data = json_decode($splfile->current());
+    return $data;
+}
+
+function jsonAll($file)
+{
+    $entries = file($file);
+    $array = [];
+    foreach($entries as $entry)
+    {
+        $array[] = json_decode($entry);
+    }
+    return $array;
+}
+
+function jsonNextID($file)
+{
+    $splfile = new SplFileObject($file);
+    $splfile->seek(PHP_INT_MAX);
+    return $splfile->key() + 1;
+}
+
+function jsonRowCount($file)
+{
+    $splfile = new SplFileObject($file);
+    $splfile->seek(PHP_INT_MAX);
+    return $splfile->key();
+}
