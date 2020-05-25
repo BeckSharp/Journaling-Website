613b25043094685ca1d866b50af1a6a37332dfbf
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-26 00:28:45 +0100

    Sign up form created

--- signUp.php
+++ signUp.php
@@ -4,8 +4,52 @@ include("api/api.inc.php");
 
 //PAGE GENERATION LOGIC
 function createPage() {
-    $content = <<<PAGE
 
+    $method = appFormMethod();
+
+    $content = <<<PAGE
+<form class="form-horizontal" method="{$method}" action="app_create_profile.php">
+    <fieldset>
+        <!-- Form Name -->
+        <legend>Sign Up Form</legend>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="username">Username</label>
+            <div class="col-md-4">
+                <input id="username" name="username" type="text" 
+                placeholder="Enter your desired username here" class="form-control input-md" required>
+            </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="password">Password</label>
+            <div class="col-md-4">
+                <input id="password" name="password" type="password" 
+                placeholder="Enter your password here" class="form-control input-md" required>
+            </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="passwordConfirmation">Confirmation</label>
+            <div class="col-md-4">
+                <input id="passwordConfirmation" name="passwordConfirmation" type="password" 
+                placeholder="Confirm your password here" class="form-control input-md" required>
+            </div>
+        </div>
+
+        <!-- Button -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="form-sub">Create Account</label>
+          <div class="col-md-4">
+            <button id="form-sub" name="form-sub" type="submit" class="btn btn-primary">Sign Up</button>
+          </div>
+        </div>
+
+    </fieldset>
+</form>
 PAGE;
     return $content;
 }
