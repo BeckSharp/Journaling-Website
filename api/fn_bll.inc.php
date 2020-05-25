486fd20a1972de3d82a41d533874267534215006
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-25 23:48:24 +0100

    appProfileRegisteredCheck() made reusable

--- api/fn_bll.inc.php
+++ api/fn_bll.inc.php
@@ -2,7 +2,9 @@
 //FUNCTION TO REDIRECT USER IF ACCOUNT NOT REGISTERED
 function appProfileRegisteredCheck() {
     $profile = jsonLoadProfile();
-    if (is_null($profile->username)) { header('Location: signUp.php'); }
+    if (is_null($profile->username)) { return false; }
+    if (is_null($profile->password)) { return false; }
+    return true;
 }
 
 function appRedirect($address) {
