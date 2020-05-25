6f0f3ee40411ed8d968c7e79984f3e662c75e255
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-25 17:33:47 +0100

    Object classes created

--- api/oo_bll.inc.php
+++ api/oo_bll.inc.php
@@ -1,1 +1,37 @@
 <?php
+//OBJECT CLASSES
+class Profile {
+    //FIELDS
+    public $username;
+    public $password;
+
+    //FUNCTIONS
+    public function fromArray(stdClass $assoc)
+    {
+        foreach($assoc as $key => $value)
+        {
+            $this->{$key} = $value;
+        }
+    }
+}
+
+class JournalEntry {
+    //FIELDS
+    public $username;
+    public $date;
+    public $weeding;
+    public $reflection;
+    public $planning;
+    public $noteTaking;
+    public $questions;
+
+    //FUNCTIONS
+    public function fromArray(stdClass $assoc)
+    {
+        foreach($assoc as $key => $value)
+        {
+            $this->{$key} = $value;
+        }
+    }
+}
+?>
