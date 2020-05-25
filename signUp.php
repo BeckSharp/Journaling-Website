deb93a21c7c06b75b432915bb17e763528fc1336
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-25 23:52:09 +0100

    Base code for sign up page implemented

--- signUp.php
+++ signUp.php
@@ -1,1 +1,30 @@
 <?php
+//INCLUDING API
+include("api/api.inc.php");
+
+//PAGE GENERATION LOGIC
+function createPage() {
+    $content = <<<PAGE
+
+PAGE;
+    return $content;
+}
+
+//BUSINESS LOGIC
+if(appProfileRegisteredCheck()) {
+    appRedirect("logIn.php");
+}
+
+$pagetitle = "Sign Up";
+$pagelead  = "";
+$pagecontent = createPage();
+$pagefooter = "";
+
+//BUILDING HTML PAGE
+$page = new MasterPage($pagetitle);
+if(!empty($pagelead))
+    $page->setDynamic1($pagelead);
+$page->setDynamic2($pagecontent);
+if(!empty($pagefooter))
+    $page->setDynamic3($pagefooter);
+$page->renderPage();
