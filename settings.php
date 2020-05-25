ef298f1e219d6bdf45f99c6b12b7c2ea76877d7c
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-25 16:55:58 +0100

    Base code added to the two new pages

---
+++ settings.php
@@ -0,0 +1,26 @@
+<?php
+//INCLUDING API
+include("api/api.inc.php");
+
+//PAGE GENERATION LOGIC
+function createPage() {
+    $content = <<<PAGE
+
+PAGE;
+    return $content;
+}
+
+//BUSINESS LOGIC
+$pagetitle = "Settings";
+$pagelead  = "";
+$pagecontent = createPage();
+$pagefooter = "";
+
+//BUILDING HTML PAGE
+$page = new MasterPage($pagetitle);
+if(!empty($pagelead))
+    $page->setDynamic1($pagelead);
+$page->setDynamic2($pagecontent);
+if(!empty($pagefooter))
+    $page->setDynamic3($pagefooter);
+$page->renderPage();
