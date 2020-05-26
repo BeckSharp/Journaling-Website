aebdcc408573945aa2ee7ff8e608aaf119baa1ad
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-27 00:29:35 +0100

    transfer of app_create_profile.php code

--- signUp.php
+++ signUp.php
@@ -18,9 +18,30 @@ if(appProfileRegisteredCheck()) {
     appRedirect("logIn.php");
 }
 
+if (appFormMethodIsPost()){
+    //RETRIEVING POSTED DATA
+    $username = $_POST["username"] ?? "";
+    $password = $_POST["password"] ?? "";
+    $confirmation = $_POST["passwordConfirmation"] ?? "";
+
+    //REMOVING MALICIOUS TEXT
+    $username = appReplaceEntityTags($username);
+    $password = appReplaceEntityTags($password);
+    $confirmation = appReplaceEntityTags($confirmation);
+
+    if (isDataValid($username, $password, $confirmation)) {
+        //ENCRYPT USERNAME
+        //HASH & SALT PASSWORD
+        //WRITE DATA TO JSON
+    } else {
+        //REDIRECT TO signUp.php WITH ERROR MESSAGES
+    }
+} else {
+    $pagecontent = createPage();
+}
+
 $pagetitle = "Sign Up";
 $pagelead  = "";
-$pagecontent = createPage();
 $pagefooter = "";
 
 //BUILDING HTML PAGE
@@ -31,3 +52,22 @@ $page->setDynamic2($pagecontent);
 if(!empty($pagefooter))
     $page->setDynamic3($pagefooter);
 $page->renderPage();
+
+//FUNCTIONS TO VALIDATE DATA
+function isDataValid($username, $password, $confirmation) {
+    if (!isDataNotEmpty($username, $password, $confirmation)) { return false; }
+    if (!isPasswordConfirmed($password, $confirmation)) { return false; }
+    return true;
+}
+
+function isDataNotEmpty($username, $password, $confirmation) {
+    if (empty($username)) { return false; }
+    if (empty($password)) { return false; }
+    if (empty($confirmation)) { return false; }
+    return true;
+}
+
+function isPasswordConfirmed($password, $confirmation) {
+    if ($password != $confirmation) { return false; }
+    return true;
+}
