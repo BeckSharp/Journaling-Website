0ffaa257adb27e6d2e0264ed604c0ca2e556d584
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-26 21:11:34 +0100

    Validation logic added for signing up

--- app_create_profile.php
+++ app_create_profile.php
@@ -1,1 +1,40 @@
 <?php
+//RETRIEVING POSTED DATA
+$username = $_POST["username"] ?? "";
+$password = $_POST["password"] ?? "";
+$confirmation = $_POST["passwordConfirmation"] ?? "";
+
+//REDIRECT IF ACCOUNT REGISTERED 
+//OR PAGE VISITED
+if (appProfileRegisteredCheck()) {
+    //REDIRECT TO logIn.php
+} 
+
+//REGISTERING ACCOUNT LOGIC
+if (isDataValid($username, $password, $confirmation)) {
+    //ENCRYPT USERNAME
+    //HASH & SALT PASSWORD
+    //WRITE DATA TO JSON
+} else {
+    //REDIRECT TO signUp.php WITH ERROR MESSAGES
+}
+
+//FUNCTIONS TO VALIDATE DATA
+function isDataValid($username, $password, $confirmation) {
+    if (!isDataNotEmpty($username, $password, $confirmation)) { return false; }
+    if (!isPasswordConfirmed($password, $confirmation)) { return false; }
+    return true;
+}
+
+function isDataNotEmpty($username, $password, $confirmation) {
+    if (empty($username)) { return false; }
+    if (empty($password)) { return false; }
+    if (empty($confirmation)) { return false; }
+    return true;
+}
+
+function isPasswordConfirmed($password, $confirmation) {
+    if ($password != $confirmation) { return false; }
+    return true;
+}
+
