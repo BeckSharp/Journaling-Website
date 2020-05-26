555e656b0455686c6af16a753cd52ac234aba7d3
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-26 21:56:29 +0100

    Rendering of sign up form moved to pl

--- api/fn_pl.inc.php
+++ api/fn_pl.inc.php
@@ -1,1 +1,50 @@
 <?php
+//RENDER FORM FUNCTIONS
+function renderFormSignUp(){
+    $method = appFormMethod();
+    $form = <<<FORM
+<form class="form-horizontal" method="{$method}" action="app_create_profile.php">
+    <fieldset>
+        <!-- Form Name -->
+        <legend>Sign Up Form</legend>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="username">Username</label>
+            <div class="col-md-4">
+                <input id="username" name="username" type="text" 
+                placeholder="Enter your desired username here" class="form-control input-md" required>
+            </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="password">Password</label>
+            <div class="col-md-4">
+                <input id="password" name="password" type="password" 
+                placeholder="Enter your password here" class="form-control input-md" required>
+            </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="passwordConfirmation">Confirmation</label>
+            <div class="col-md-4">
+                <input id="passwordConfirmation" name="passwordConfirmation" type="password" 
+                placeholder="Confirm your password here" class="form-control input-md" required>
+            </div>
+        </div>
+
+        <!-- Button -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="form-sub">Create Account</label>
+          <div class="col-md-4">
+            <button id="form-sub" name="form-sub" type="submit" class="btn btn-primary">Sign Up</button>
+          </div>
+        </div>
+
+    </fieldset>
+</form>
+FORM;
+    return $form;
+}
