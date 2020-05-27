ab741f6e8d37aeea981968586db843277383fce1
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-27 16:56:55 +0100

    BLLEncryption class added

--- api/oo_bll.inc.php
+++ api/oo_bll.inc.php
@@ -6,10 +6,8 @@ class BLLProfile {
     public $password;
 
     //FUNCTIONS
-    public function fromArray(stdClass $assoc)
-    {
-        foreach($assoc as $key => $value)
-        {
+    public function fromArray(stdClass $assoc) {
+        foreach($assoc as $key => $value) {
             $this->{$key} = $value;
         }
     }
@@ -26,12 +24,38 @@ class BLLJournalEntry {
     public $questions;
 
     //FUNCTIONS
-    public function fromArray(stdClass $assoc)
-    {
-        foreach($assoc as $key => $value)
-        {
+    public function fromArray(stdClass $assoc) {
+        foreach($assoc as $key => $value) {
             $this->{$key} = $value;
         }
+    }
+}
+
+class BLLEncryption {
+    //FIELDS
+    public $data;
+    public $ciphering;
+    public $key;
+    public $options;
+    public $initializationVector;
+
+    //Functions
+    public function __construct($data, $key) {
+        $this->data = $data;
+        $this->ciphering = "AES-128-CTR";
+        $this->key = $key;
+        $this->options = 0;
+        $this->initializationVector = '1234567891011121';
+    }
+
+    public function encryptData() {
+        return openssl_encrypt($this->data, $this->$ciphering, $this->key, 
+                                $this->options, $initializationVector);
+    }
+
+    public function decryptData() {
+        return openssl_decrypt($this->data, $this->$ciphering, $this->key, 
+                                $this->options, $initializationVector);
     }
 }
 ?>
