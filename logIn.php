ae54e8ba40b6915df1bdf42de4971838478af5ed
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-05-28 00:24:28 +0100

    Validation logic for logging in finnished

--- logIn.php
+++ logIn.php
@@ -19,9 +19,29 @@ if(!appProfileRegisteredCheck()) {
     appRedirect("signUp.php");
 }
 
+if (appFormMethodIsPost()) {
+    //RETRIEVING POSTED DATA
+    $username = $_POST["username"] ?? "";
+    $password = $_POST["password"] ?? "";
+
+    //REMOVING MALICIOUS TEXT
+    $username = appReplaceEntityTags($username);
+    $password = appReplaceEntityTags($password);
+
+    if (isLogInValid($username, $password)) {
+        //SET SESSION TOKENS
+        //REDIRECT USER TO index.php
+        appRedirect("index.php");
+    } else {
+        //REDIRECT TO logIn.php WITH ERROR MESSAGE
+        appRedirect("logIn.php?invalid=true");
+    }
+} else {
+    $pagecontent = createPage();
+}
+
 $pagetitle = "Log In";
 $pagelead  = "";
-$pagecontent = createPage();
 $pagefooter = "";
 
 //BUILDING HTML PAGE
@@ -32,3 +52,23 @@ $page->setDynamic2($pagecontent);
 if(!empty($pagefooter))
     $page->setDynamic3($pagefooter);
 $page->renderPage();
+
+//FUNCTIONS TO VALIDATE DATA
+function isLogInValid($username, $password) {
+    $profile = jsonLoadProfile();
+    if (!isUsernameValid($username, $profile->username)) { return false; }
+    if (!isPasswordValid($password, $profile->password)) { return false; }
+    return true;
+}
+
+function isUsernameValid($username, $registeredUsername) {
+    $encryption = new BLLEncryption($username, $username);
+    $encryptedUsername = $encryption->encryptData();
+    if ($encryptedUsername != $registeredUsername) { return false; }
+    return true;
+}
+
+function isPasswordValid($password, $registeredPassword) {
+    if (!password_verify($password, $registeredPassword)) { return false; }
+    return true;
+}
