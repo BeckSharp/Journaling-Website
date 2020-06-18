6b0bf4d7682ee2ce449fede7b0fc76487257cdb6
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-06-18 04:40:12 +0100

    Journal entry creation logic plan added

--- app_create_journal_entry.php
+++ app_create_journal_entry.php
@@ -24,7 +24,32 @@ if (appFormMethodIsPost() && appSessionIsSet()) {
     $planning = appReplaceEntityTags($planning);
     $notes = appReplaceEntityTags($notes);
     $question = appReplaceEntityTags($question);
-    
+
+    if (isJournalEntryValid($dateDay, $dateMonth, $dateDay, $weeding, $reflection, $planning, $notes, $question)){
+        //CREATE DATE USING POSTED DATA
+
+        //ENCRYPTING DATA FOR STORAGE
+
+        //CREATING JOURNAL ENTRY OBJECT
+        $journalEntry = new BLLJournalEntry;
+
+        //WRITE DATA TO JSON
+
+        //REDIRECT USER WITH SUCCESS MESSAGE
+
+    }
+    else {
+        //REDIRECT USER WITH ERROR MESSAGE
+        
+    }
 } else {
     appRedirect("app_error.php");
+}
+
+//FUNCTIONS TO VALIDATE DATA
+function isJournalEntryValid($day, $month, $year, $weeding, $reflection, $planning, $notes, $question) {
+    //VALIDATE DATA ISN'T EMPTY
+    //VALIDATE DATA'S DATE IS REAL
+    //VALIATE DATA'S DATE ISN'T TAKEN
+    return true;
 }
