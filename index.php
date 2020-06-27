fa53b7fea41b60116d2509b59cd4b2eb1d6524e9
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-06-27 22:11:47 +0100

    Journal output completed

--- index.php
+++ index.php
@@ -9,15 +9,39 @@ function createPage() {
     $journalOutput = "";
 
     if (count($journalData) == 0) {
+        //PRODUCING ERROR MESSAGE
         $journalOutput = file_get_contents("data/static/index/index_error_no_data.html");
     } else {
-        //RENDER JOURNAL DATA PRESENTATION
+        //DECRYPTING USER'S JOURNAL DATA
+        $key = appDecryptSessionData($_SESSION["username"]);
+        $journalData = decryptJournalEntries($journalData, $key);
+
+        //RENDERING THE JOURNAL OUTPUT DATA
+        $journalOutput = createJournalOutput($journalData);
     }
 
     $content = <<<PAGE
 {$journalOutput}
 PAGE;
     return $content;
+}
+
+function decryptJournalEntries($journalData, $key) {
+    foreach ($journalData as $entry) {
+        $entry = appDecryptJournal($entry, $key);
+    }
+    return $journalData;
+}
+
+function createJournalOutput($journalData) {
+    $count = 0;
+    $journalOutput = "<div class=\"panel-group\" id=\"accordian\">";
+    foreach ($journalData as $entry) {
+        $journalOutput .= renderJournalEntryData($entry, $count);
+        $count++;
+    }
+    $journalOutput .= "</div>";
+    return $journalOutput;
 }
 
 //BUSINESS LOGIC
